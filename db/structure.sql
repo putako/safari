1d9efd085470656c13509a27c22995c1cdc0d0cf
Jared Harless <Jared.e.harless@gmail.com>
2016-09-25 13:56:37 -0700

    made nice search page results formatting meaningless word salad

--- db/structure.sql
+++ db/structure.sql
@@ -172,6 +172,27 @@ ALTER TABLE ONLY users
 
 
 --
+-- Name: customers_lower_email; Type: INDEX; Schema: public; Owner: -; Tablespace: 
+--
+
+CREATE INDEX customers_lower_email ON customers USING btree (lower((email)::text));
+
+
+--
+-- Name: customers_lower_first_name; Type: INDEX; Schema: public; Owner: -; Tablespace: 
+--
+
+CREATE INDEX customers_lower_first_name ON customers USING btree (lower((first_name)::text) varchar_pattern_ops);
+
+
+--
+-- Name: customers_lower_last_name; Type: INDEX; Schema: public; Owner: -; Tablespace: 
+--
+
+CREATE INDEX customers_lower_last_name ON customers USING btree (lower((last_name)::text) varchar_pattern_ops);
+
+
+--
 -- Name: index_customers_on_email; Type: INDEX; Schema: public; Owner: -; Tablespace: 
 --
 
@@ -205,6 +226,6 @@ CREATE UNIQUE INDEX index_users_on_reset_password_token ON users USING btree (re
 
 SET search_path TO "$user",public;
 
-INSERT INTO schema_migrations (version) VALUES ('20160924233834'), ('20160925014411'), ('20160925023327');
+INSERT INTO schema_migrations (version) VALUES ('20160924233834'), ('20160925014411'), ('20160925023327'), ('20160925200935');
 
 
