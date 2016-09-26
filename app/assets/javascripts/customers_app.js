6c944119243d49796e389254105b38fa28302def
Jared Harless <Jared.e.harless@gmail.com>
2016-09-26 16:30:27 -0700

    now responding to searches with results, AJAX JSON style

--- app/assets/javascripts/customers_app.js
+++ app/assets/javascripts/customers_app.js
@@ -1,11 +1,20 @@
 var app = angular.module('customers',[]);
 
-var CustomerSearchController = function($scope){
-	$scope.search = function(searchTerm){
-		$scope.searchedFor = searchTerm;
-	}
-}
-
 app.controller("CustomerSearchController",
-				["$scope", CustomerSearchController]
-);
+				["$scope", "$http",
+			function($scope, $http){
+		$scope.customers = [];
+		$scope.search = function(searchTerm){
+			$http.get("/customers.json",
+				 { "params": { "keywords": searchTerm } }
+				).then(
+					function(response) {//true
+						$scope.customers = response.data;
+					},
+					function(response) {//false
+						alert("Dave's not here, man. " + response.status);
+					}
+				);
+		}
+	}
+]);
