abe3340624e97bb6c90ed643cba33ace2baca0f3
Jared Harless <Jared.e.harless@gmail.com>
2016-09-25 18:09:49 -0700

    started adding some angular before bedtime

---
+++ app/assets/javascripts/customers_app.js
@@ -0,0 +1,11 @@
+var app = angular.module('customers',[]);
+
+var CustomerSearchController = function($scope){
+	$scope.search = function(searchTerm){
+		$scope.searchedFor = searchTerm;
+	}
+}
+
+app.controller("CustomerSearchController",
+				["$scope", CustomerSearchController]
+);
