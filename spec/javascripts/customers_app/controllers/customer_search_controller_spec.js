aef812bc64e4f2353d6008b5513da463bf3d046d
Jared Harless <Jared.e.harless@gmail.com>
2016-10-03 15:57:58 -0700

    refactored some of the test specs

--- spec/javascripts/customers_app/controllers/customer_search_controller_spec.js
+++ spec/javascripts/customers_app/controllers/customer_search_controller_spec.js
@@ -1,10 +1,27 @@
 describe("CustomerSearchController", function(){
 
-	describe("SHIT THE FUCKIN BED NAGGAAAAHHHH", function(){
-		var scope = null,
-		controller = null,
-		httpBackend = null,
-		serverResults = [
+	describe("Initialization", function(){
+		var scope 		= null,
+			controller 	= null;
+		beforeEach(module("customers"));
+		beforeEach(inject(function($controller, $rootScope){
+			scope 		= $rootScope.$new();
+			controller 	= $controller("CustomerSearchController", {
+				$scope: scope
+			});
+		}));
+
+		//tests go here
+		it("defaults to an empty customer list", function(){
+			expect(scope.customers).toEqualData([]);
+		});
+	}); // end of initialization section
+
+	describe("Fetching Search Results", function(){
+		var scope 		= null,
+			controller 	= null,
+			httpBackend = null,
+			serverResults = [
 				{
 					id: 123,
 					first_name: "Bob",
@@ -22,43 +39,66 @@ describe("CustomerSearchController", function(){
 			];
 
 		beforeEach(module("customers"));
-
 		beforeEach(inject(function($controller, $rootScope, $httpBackend){
-			scope = $rootScope.$new();
+			scope 		= $rootScope.$new();
 			httpBackend = $httpBackend;
-			controller = $controller("CustomerSearchController", {
+			controller  = $controller("CustomerSearchController", {
 				$scope: scope
 			});
 		}));
-
-		/*
-		beforeEach(function(){ //this is to fake some real results, success for lotion
+		beforeEach(function(){
 			httpBackend.when('GET','/customers.json?keywords=bob&page=0').
 				respond(serverResults);
 		});
-		*/
-		beforeEach(function(){ //this is to fake some real failures, success for hose
-			httpBackend.when('GET','/customers.json?keywords=bob&page=0').
-				respond(500,'Internal Server Error');
-			spyOn(window,"alert");
-		});
-
-		// tests go here
-		it("defaults to an empty customer list", function(){
-			expect(scope.customers).toEqualData([]);
-		});
-
-		it("puts the lotion on its skin", function(){
+		//tests
+		it("populates the customer list with the results", function(){
 			scope.search("bob");
 			httpBackend.flush();
 			expect(scope.customers).toEqualData(serverResults);
 		});
+	}); //end of "fetching search results"
 
-		it("gets the hose again", function(){
+	describe("Error Handling", function(){
+		var scope 		= null,
+			controller 	= null,
+			httpBackend = null/*,
+			serverResults = [
+				{
+					id: 123,
+					first_name: "Bob",
+					last_name: "Jones",
+					email: "bjones@foo.net",
+					username: "jonesy"
+				},
+				{
+					id: 456,
+					first_name: "Bob",
+					last_name: "Johnsons",
+					email: "johnboy@bar.info",
+					username: "bobbyj"
+				}
+			]*/;
+
+		beforeEach(module("customers"));
+		beforeEach(inject(function($controller, $rootScope, $httpBackend){
+			scope 		= $rootScope.$new();
+			httpBackend = $httpBackend;
+			controller  = $controller("CustomerSearchController", {
+				$scope: scope
+			});
+		}));
+		beforeEach(function(){
+			httpBackend.when('GET','/customers.json?keywords=bob&page=0').
+				respond(500, 'Internal Server Error');
+			spyOn(window, "alert");
+		});
+		//tests go here
+		it("alerts the user on an error", function(){
 			scope.search("bob");
 			httpBackend.flush();
 			expect(scope.customers).toEqualData([]);
-			expect(window.alert).toHaveBeenCalledWith("Dave's not here man. 500");
+			expect(window.alert).toHaveBeenCalledWith(
+				"Dave's not here man. 500");
 		});
-	});
+	}); //end of "error handling"
 });
